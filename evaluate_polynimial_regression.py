86b90769ef0b3dcb9762bb0fc5d65dbd4a91b5b0
Kiarash Kiani <kiarash.kiani7997@gmail.com>
2024-05-15 22:56:31 +0330

    split the data set and plot it and save the fig as png

---
+++ evaluate_polynimial_regression.py
@@ -0,0 +1,12 @@
+from utils import *
+
+# Generate some data
+X,y,x_ideal,y_ideal = gen_data(18, 2, 0.7)
+# X.shape (18,) y.shape (18,)
+
+""" Split the data using sklearn train_test_split """
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+# X_train.shape (12,) y_train.shape (12,)
+# X_test.shape (6,) y_test.shape (6,)
+
+plot_train_and_test_sets(x_ideal, y_ideal, X_train, X_test, y_train, y_test)
--- utils.py
+++ utils.py
@@ -1,5 +1,30 @@
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+# %matplotlib widget
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.activations import relu,linear
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+
+from public_tests import * 
+
+tf.keras.backend.set_floatx('float64')
+from utils import *
+
+tf.autograph.set_verbosity(0)
+#! ---------------------------------------------------------------------------------
 
 """ generate a data set based on a x^2 with added noise """
 def gen_data(m, seed=1, scale=0.7):
@@ -10,3 +35,20 @@ def gen_data(m, seed=1, scale=0.7):
     y_train = y_ideal + scale * y_ideal*(np.random.sample((m,))-0.5)
     x_ideal = x_train #for redraw when new data included in X
     return x_train, y_train, x_ideal, y_ideal
+
+
+""" Plot the training and test sets """
+def plot_train_and_test_sets(x_ideal, y_ideal, X_train, X_test, y_train, y_test):
+    fig, ax = plt.subplots(1,1,figsize=(4,4))
+    ax.plot(x_ideal, y_ideal, "--", color = "orangered", label="y_ideal", lw=1)
+    ax.set_title("Training, Test",fontsize = 14)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+
+    ax.scatter(X_train, y_train, color = "red",           label="train")
+    ax.scatter(X_test, y_test,   color = "blue",   label="test")
+    ax.legend(loc='upper left')
+    
+     # Save the figure
+    
+    fig.savefig('images/train-test_plot.png')
